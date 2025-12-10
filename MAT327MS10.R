ed3b2dcb387dd131cf239ab6f42b095a8d402490
luzsepulveda347-jpg <luzsepulveda347@gmail.com>
2025-12-09 21:34:54 -0500

    Add files via upload

---
+++ MAT327MS10.R
@@ -0,0 +1,56 @@
+#Physical Activity
+View(sleep)
+data <- c(sleep)
+xbar <- sd(data)
+xbar <- sd(sleep)
+data <- c(sleep$`Physical Activity Level`)
+xbar <- mean(sleep$`Physical Activity Level`)
+mean(sleep$`Physical Activity Level`)
+s <- sd(sleep$`Physical Activity Level`)
+sd(sleep$`Physical Activity Level`)
+n <- 374
+t <- qt(0.95, 374-1)
+qt(0.95, 374-1)
+L <- xbar - t*s/sqrt(n)
+xbar - t*s/sqrt(n)
+U = xbar +t*s/sqrt(n)
+xbar +t*s/sqrt(n)
+t <- -qt(0.025, 374-1)
+-qt(0.025, 374-1)
+
+
+#Stress Levels
+View(sleep)
+data <- c(sleep$`Stress Level`)
+xbar <- mean(sleep$`Stress Level`)
+mean(sleep$`Stress Level`)
+s <- sd(sleep$`Stress Level`)
+sd(sleep$`Stress Level`)
+n <- 374
+t <- qt(0.95, 374-1)
+qt(0.95, 374-1)
+L <- xbar - t*s/sqrt(n)
+xbar - t*s/sqrt(n)
+U = xbar +t*s/sqrt(n)
+xbar +t*s/sqrt(n)
+t <- -qt(0.025, 374-1)
+-qt(0.025, 374-1)
+
+
+#Heart Rate
+View(sleep)
+data <- c(sleep$`Heart Rate`)
+xbar <- mean(sleep$`Heart Rate`)
+mean(sleep$`Heart Rate`)
+s <- sd(sleep$`Heart Rate`)
+sd(sleep$`Heart Rate`)
+n <- 374
+t <- qt(0.95, 374-1)
+qt(0.95, 374-1)
+L <- xbar - t*s/sqrt(n)
+xbar - t*s/sqrt(n)
+U = xbar +t*s/sqrt(n)
+xbar +t*s/sqrt(n)
+t <- -qt(0.025, 374-1)
+-qt(0.025, 374-1)
+
